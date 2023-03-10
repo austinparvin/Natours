87f082c609b2750f747a6965dfb42fb864d383e2
Austin Parvin <austin.parvin@gmail.com>
2023-03-10 12:31:44 -0600

    simple server setup

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-// Blocking Synchronous way
-const textIn = fs.readFileSync(__dirname + "/txt/input.txt", "utf8");
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync(__dirname + "/txt/input.txt", textOut);
-console.log("Text written!");
+/*********************************************     FILES     *********************************************/
 
-// Non-blocking asynchronous way
+// // Blocking Synchronous way
+// const textIn = fs.readFileSync(__dirname + "/txt/input.txt", "utf8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync(__dirname + "/txt/input.txt", textOut);
+// console.log("Text written!");
 
-// Callback hell!
-fs.readFile(__dirname + "/txt/start.txt", "utf8", (err, data1) => {
-    if (err) return console.log("ERROR!");
-    fs.readFile(__dirname + `/txt/${data1}.txt`, "utf8", (err, data2) => {
-        console.log("[Asimo] data2:", data2);
-        fs.readFile(__dirname + `/txt/append.txt`, "utf8", (err, data3) => {
-            console.log("[Asimo] data3:", data3);
-            fs.writeFile(
-                __dirname + "/txt/final.txt",
-                `${data2}\n${data3}`,
-                "utf-8",
-                (err) => {
-                    console.log("[Asimo] File has been written!");
-                }
-            );
-        });
-    });
+// // Non-blocking asynchronous way
+
+// // Callback hell!
+// fs.readFile(__dirname + "/txt/start.txt", "utf8", (err, data1) => {
+//     if (err) return console.log("ERROR!");
+//     fs.readFile(__dirname + `/txt/${data1}.txt`, "utf8", (err, data2) => {
+//         console.log("[Asimo] data2:", data2);
+//         fs.readFile(__dirname + `/txt/append.txt`, "utf8", (err, data3) => {
+//             console.log("[Asimo] data3:", data3);
+//             fs.writeFile(
+//                 __dirname + "/txt/final.txt",
+//                 `${data2}\n${data3}`,
+//                 "utf-8",
+//                 (err) => {
+//                     console.log("[Asimo] File has been written!");
+//                 }
+//             );
+//         });
+//     });
+// });
+// console.log("[Asimo] Non-blocking asynchronous way");
+
+/*********************************************     SERVER   *********************************************/
+
+const server = http.createServer((req, res) => {
+    res.end("Hello from the server");
 });
-console.log("[Asimo] Non-blocking asynchronous way");
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("[Asimo] Server has started!");
+});
