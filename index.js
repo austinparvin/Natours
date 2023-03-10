3ade657c179a35f9293a5430ffb2dd4986110ea9
Austin Parvin <austin.parvin@gmail.com>
2023-03-10 14:46:38 -0600

    move replaceTemplate to own module

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const { replaceTemplate } = require("./modules/replaceTemplate");
+
 /*********************************************     FILES     *********************************************/
 
 // // Blocking Synchronous way
@@ -35,23 +37,6 @@ const url = require("url");
 
 /*********************************************     SERVER   *********************************************/
 
-const replaceTemplate = (temp, product) => {
-    let output = temp
-        .replace(/{%PRODUCT_NAME%}/g, product.productName)
-        .replace(/{%IMAGE%}/g, product.image)
-        .replace(/{%PRICE%}/g, product.price)
-        .replace(/{%FROM%}/g, product.from)
-        .replace(/{%NUTRIENTS%}/g, product.nutrients)
-        .replace(/{%QUANTITY%}/g, product.quantity)
-        .replace(/{%DESCRIPTION%}/g, product.description)
-        .replace(/{%ID%}/g, product.id);
-
-    if (!product.organic)
-        output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-    else output = output.replace(/{%NOT_ORGANIC%}/g, "organic");
-
-    return output;
-};
 const templateOverview = fs.readFileSync(
     `${__dirname}/templates/template-overview.html`,
     "utf-8"
