7aff43fe3f2d47ef34d2ba6727fb2c257fe1193e
Austin Parvin <austin.parvin@gmail.com>
2023-03-12 12:43:39 -0500

    pre async await

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ fs.readFile('./dog.txt', (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      console.log(res.body);
+    .then((res) => {
+      console.log(res.body.message);
 
       fs.writeFile('./dog-image.txt', res.body.message, (err) => {
         if (err) return console.log(err);
         console.log('dog image saved');
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
