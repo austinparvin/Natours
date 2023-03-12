97ea0c9a0e756de3caff124c8bc5905b98225056
Austin Parvin <austin.parvin@gmail.com>
2023-03-12 14:01:42 -0500

    refactor callback hell into chained promises using .then()

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile('./dog.txt', (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('./dog-image.txt', res.body.message, (err) => {
-        if (err) return console.log(err);
-        console.log('dog image saved');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find that file 🤕');
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject(err);
+      resolve('File written ✏️');
+    });
+  });
+};
+
+readFilePromise('./dog.txt')
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePromise('./dog-image.txt', res.body.message);
+  })
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(`Error ❌: ${err.message}`);
+  });
