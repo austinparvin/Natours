c9ccea3956994aaa21d6a61547e792deec6bba17
Austin Parvin <austin.parvin@gmail.com>
2023-03-12 14:24:49 -0500

    use async/await in place of .then chaining

--- index.js
+++ index.js
@@ -19,18 +19,46 @@ const writeFilePromise = (file, data) => {
   });
 };
 
-readFilePromise('./dog.txt')
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
     console.log(res.body.message);
-    return writeFilePromise('./dog-image.txt', res.body.message);
-  })
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
+    await writeFilePromise(`${__dirname}/dog-image.txt`, res.body.message);
+    console.log(`File written 👍`);
+  } catch (err) {
+    throw `Error ❌: ${err.message}`;
+  }
+
+  return '2: Ready 🐶';
+};
+
+(async () => {
+  try {
+    console.log('1: Will get dog pic ⏲');
+    const result = await getDogPic();
+    console.log(result);
+    console.log('3: Got dog pic 📷');
+  } catch (err) {
     console.log(`Error ❌: ${err.message}`);
-  });
+  }
+})();
+
+// readFilePromise('./dog.txt')
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePromise('./dog-image.txt', res.body.message);
+//   })
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(`Error ❌: ${err.message}`);
+//   });
