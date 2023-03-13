a645820f4ba9d0cf5667a86346c51760ffdc9d08
Austin Parvin <austin.parvin@gmail.com>
2023-03-13 13:59:26 -0500

    add post method for tours

--- app.js
+++ app.js
@@ -30,6 +30,26 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
