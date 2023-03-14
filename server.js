21368303c6d6b8a38245a9f69fe90fef91b90f25
Austin Parvin <austin.parvin@gmail.com>
2023-03-13 20:13:13 -0500

    dotenv

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
 
+console.log('[Austin] process.env:', process.env);
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
