07c8412c45e171508a9b01121ca88e0f082660a7
Austin Parvin <austin.parvin@gmail.com>
2023-03-13 20:33:08 -0500

    eslint and process.env

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const app = require('./app');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config();
 
-console.log('[Austin] process.env:', process.env);
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
