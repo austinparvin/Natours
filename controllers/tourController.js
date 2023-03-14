7d552db62483a4f4a3b1ce081a033590f46bd26f
Austin Parvin <austin.parvin@gmail.com>
2023-03-14 13:20:17 -0500

    include simple filter on getAllTours, define node version in npm

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,13 @@ const Tour = require('../models/tourModel');
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find({});
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
