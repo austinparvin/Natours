9f3f780543ba7d899ff904755e48910aa8e0a022
Austin Parvin <austin.parvin@gmail.com>
2023-03-15 09:47:36 -0500

    global error handling middleware

--- app.js
+++ app.js
@@ -21,11 +21,22 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Cannot find ${req.originalUrl} on the server. ❌`,
+  const err = new Error(`Cannot find ${req.originalUrl} on the server. ❌`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
-  next();
 });
 
 module.exports = app;
