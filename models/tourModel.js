8ac2857248aa2ed8d62d6aa85d78bbad65668c4a
Austin Parvin <austin.parvin@gmail.com>
2023-03-15 06:02:51 -0500

    Document middleware, post and pre chain

--- models/tourModel.js
+++ models/tourModel.js
@@ -77,6 +77,18 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+// eslint-disable-next-line prefer-arrow-callback
+tourSchema.pre('save', function (next) {
+  console.log('[Austin] Will save document...');
+  next();
+});
+
+// eslint-disable-next-line prefer-arrow-callback
+tourSchema.post('save', function (doc, next) {
+  console.log('[Austin] doc:', doc);
+  next();
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
