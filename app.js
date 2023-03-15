6dbff7caf265b4b2e5ec5d96a00ecbbf4a36b567
Austin Parvin <austin.parvin@gmail.com>
2023-03-15 09:15:51 -0500

    unhandled route error handling

--- app.js
+++ app.js
@@ -13,15 +13,19 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('[Austin] Hello from the middleware 👋');
-  next();
-});
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cannot find ${req.originalUrl} on the server. ❌`,
+  });
+  next();
+});
 
 module.exports = app;
