383aed8b4db1d6ab2da8b82dddd5c386cb2bd6ad
Austin Parvin <austin.parvin@gmail.com>
2023-03-16 07:13:07 -0500

    create and return jwt token on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,23 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 const signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
-    email: req.body.password,
+    email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
