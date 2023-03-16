aa4f1935175a7c17aac927de68da141ebaa8423a
Austin Parvin <austin.parvin@gmail.com>
2023-03-16 06:47:16 -0500

    validate and encrypt passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -22,7 +23,20 @@ const userSchema = mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'A user must confirm their password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
