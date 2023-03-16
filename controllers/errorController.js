e965cb0def491147cce3301c5eaf7839ace18fbb
Austin Parvin <austin.parvin@gmail.com>
2023-03-16 04:49:52 -0500

    send different errors for dev vs prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,38 @@
+const sendErrorForDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorForProd = (err, res) => {
+  // Operational, trusted error: send to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    console.log('[Austin] err 💥:', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorForDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorForProd(err, res);
+  }
 };
