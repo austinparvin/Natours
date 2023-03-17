39fb3651952c7a7b8d6ed16ad61e0410dbf3168d
Austin Parvin <austin.parvin@gmail.com>
2023-03-17 07:02:41 -0500

    include helmet for securing http headers + comments

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -15,23 +16,39 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 
-app.use('/api/', limiter);
+// MIDDLEWARE STACK
+// Set security HTTP headers
+app.use(helmet());
 
-//Middleware Stack
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Limit requests from same IP
+app.use('/api', limiter);
+
+// Body parser, reading data from the body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// Router mounting
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// 404 catch
 app.all('*', (req, res, next) => {
   const err = new AppError(
     `Cannot find ${req.originalUrl} on the server. ❌`,
