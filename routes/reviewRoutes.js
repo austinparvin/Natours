007596844bc269e96f83252aac2c8d2cf2caa19f
Austin Parvin <austin.parvin@gmail.com>
2023-03-17 13:49:34 -0500

    create reviewRouter

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+const {
+  getAllReviews,
+  createReview,
+} = require('../controllers/reviewController');
+
+const { protect, restrictTo } = require('../controllers/authController');
+// prettier-ignore
+router
+  .route('/')
+  .get(getAllReviews)
+  .post(protect, restrictTo('user'), createReview);
+
+module.exports = router;
