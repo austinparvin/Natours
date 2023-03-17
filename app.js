26424ff4498099e996056427315992c6e241a7de
Austin Parvin <austin.parvin@gmail.com>
2023-03-17 06:56:20 -0500

    implement global rate limiter via middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,6 +8,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
+
+const limiter = rateLimit({
+  max: 100,
+  windowMS: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api/', limiter);
 
 //Middleware Stack
 if (process.env.NODE_ENV === 'development') {
