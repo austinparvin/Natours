b32bbb7442dcee8a8a4d09ab26eb8c707c159436
Austin Parvin <austin.parvin@gmail.com>
2023-03-17 16:16:15 -0500

    simply getAllReviews and pass tourId if it exists as a filter

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,17 +1,14 @@
 const catchAsync = require('../utils/catchAsync');
 const Review = require('../models/reviewModel');
-const APIFeatures = require('../utils/apiFeatures');
 
 const getAllReviews = catchAsync(async (req, res, next) => {
-  const features = new APIFeatures(Review.find(), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
-  const reviews = await features.query;
+  let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+  const reviews = await Review.find(filter);
 
   res.status(200).json({
     status: 'success',
+    results: reviews.length,
     data: {
       reviews,
     },
