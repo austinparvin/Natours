f7f0812e39f48d48e7d99164c12249242478f80a
Austin Parvin <austin.parvin@gmail.com>
2023-03-17 07:26:04 -0500

    prevent NoSQL and XSS attacks

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +36,18 @@ app.use(
     limit: '10kb',
   })
 );
+
+// Data Sanitization against NoSQL query injection
+/* EX: logging in using only a password
+{ 
+  "email": { "$gt": "" },
+  "password": "common-password"
+}
+*/
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
