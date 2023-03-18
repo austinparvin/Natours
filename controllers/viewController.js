6727e9924ad88156aae494cd72d6c769723fd1d2
Austin Parvin <austin.parvin@gmail.com>
2023-03-18 13:21:05 -0500

    view controller, routes and router mounting

---
+++ controllers/viewController.js
@@ -0,0 +1,13 @@
+const getOverview = (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+};
+
+const getTour = (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+};
+
+module.exports = { getOverview, getTour };
