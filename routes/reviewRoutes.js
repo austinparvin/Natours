396f9180c77e8d1d9e207cf7dff78ae7e72094f9
Austin Parvin <austin.parvin@gmail.com>
2023-03-18 05:24:36 -0500

    add delete review route

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,13 +4,20 @@ const router = express.Router({ mergeParams: true });
 const {
   getAllReviews,
   createReview,
+  deleteReview,
 } = require('../controllers/reviewController');
 
 const { protect, restrictTo } = require('../controllers/authController');
+
 // prettier-ignore
 router
   .route('/')
   .get(getAllReviews)
   .post(protect, restrictTo('user'), createReview);
+
+// prettier-ignore
+router
+  .route('/:id')
+  .delete(protect, restrictTo('admin'), deleteReview)
 
 module.exports = router;
