9743e4e409434ba603dab9b529fd7c71519c8ec9
Austin Parvin <austin.parvin@gmail.com>
2023-03-18 05:33:37 -0500

    create update in handlerFactory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -15,6 +15,26 @@ const deleteOne = (Model) =>
     });
   });
 
+const updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new AppError('No doc found with that id', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
 module.exports = {
   deleteOne,
+  updateOne,
 };
