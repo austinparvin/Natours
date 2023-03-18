7ce9beae0b972decee832d73372004f3003357f5
Austin Parvin <austin.parvin@gmail.com>
2023-03-18 07:15:57 -0500

    aggregate averageRating stats in static fn, log res

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -46,5 +46,24 @@ reviewSchema.pre(/^find/, async function (next) {
   next();
 });
 
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  console.log('[Austin] stats:', stats);
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
