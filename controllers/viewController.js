7aae242215011cf6b7180fdf0ea8c440412dc840
Austin Parvin <austin.parvin@gmail.com>
2023-03-19 06:03:10 -0500

    update viewController

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 const getOverview = catchAsync(async (req, res) => {
   // get tour data
@@ -12,15 +13,19 @@ const getOverview = catchAsync(async (req, res) => {
   });
 });
 
-const getTour = catchAsync(async (req, res) => {
-  const tour = await Tour.findOne({
-    slug: req.params.slug,
-  }).populate({
+const getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
-    fields: 'review ratings user',
+    fields: 'review rating user',
   });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name.', 404));
+  }
+
+  console.log('[Austin] tour.guides:', tour.guides);
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
     tour,
   });
 });
