28b9aba59a020e4cf5766433b5865e5d8bca8de4
Austin Parvin <austin.parvin@gmail.com>
2023-03-20 06:55:06 -0500

    default img on new user creation

--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,10 @@ const userSchema = mongoose.Schema({
     lowercase: true,
     validate: [validator.isEmail, 'Invalid email format'],
   },
-  photo: String,
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
   role: {
     type: String,
     enum: ['user', 'guide', 'lead-guide', 'admin'],
