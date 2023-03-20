360737ae1acd3b1c673bb7e5a41bc55218428f9c
Austin Parvin <austin.parvin@gmail.com>
2023-03-20 16:47:47 -0500

    implement sendgrid for prod

--- utils/email.js
+++ utils/email.js
@@ -12,8 +12,13 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      //sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
 
     return nodemailer.createTransport({
