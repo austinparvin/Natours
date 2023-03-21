d8df085c78402872cfa3f72ce073bd6c776e6c00
Austin Parvin <austin.parvin@gmail.com>
2023-03-21 06:54:29 -0500

    get checkout session route implemented

---
+++ controllers/bookingController.js
@@ -0,0 +1,39 @@
+const Stripe = require('stripe');
+
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+const getCheckoutSession = catchAsync(async (req, res, next) => {
+  const { tourId } = req.params;
+  const tour = await Tour.findById(tourId);
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: tourId,
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
+
+module.exports = { getCheckoutSession };
