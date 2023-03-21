50f82ce1d1bd096109eb94ad9c2f7d23e64e4bd4
Austin Parvin <austin.parvin@gmail.com>
2023-03-21 10:35:04 -0500

    clean dev-data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const fs = require('fs');
+
 const Tour = require('../../models/tourModel');
 const User = require('../../models/userModel');
 const Review = require('../../models/reviewModel');
