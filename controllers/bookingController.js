d2077b95b70b7f0454547a6f9d098e1319f951fe
Austin Parvin <austin.parvin@gmail.com>
2023-03-21 08:08:54 -0500

    crete booking on succesful payment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,6 +2,16 @@ const Stripe = require('stripe');
 
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const Booking = require('../models/bookingModel');
+
+const createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour || !user || !price) return next();
+
+  await Booking.create({ tour, user, price });
+  res.redirect(req.originalUrl.split('?')[0]);
+});
 
 const getCheckoutSession = catchAsync(async (req, res, next) => {
   const { tourId } = req.params;
@@ -9,7 +19,9 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
   const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${tourId}&user=${
+      req.user.id
+    }&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: tourId,
@@ -36,4 +48,4 @@ const getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = { getCheckoutSession };
+module.exports = { getCheckoutSession, createBookingCheckout };
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,12 +8,13 @@ const {
   allowCDNScripts,
 } = require('../controllers/viewController');
 const { isLoggedIn, protect } = require('../controllers/authController');
+const { createBookingCheckout } = require('../controllers/bookingController');
 
 const router = express.Router();
 
 router.use(allowCDNScripts);
 // router.post('/submit-user-data', protect, updateUserData);
-router.get('/', isLoggedIn, getOverview);
+router.get('/', createBookingCheckout, isLoggedIn, getOverview);
 router.get('/tour/:slug', isLoggedIn, getTour);
 router.get('/login', isLoggedIn, getLoginForm);
 router.get('/me', protect, getAccount);
