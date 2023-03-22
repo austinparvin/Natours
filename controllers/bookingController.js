6957ecc708551d287b732c2a48911e471677839d
Austin Parvin <austin.parvin@gmail.com>
2023-03-22 05:57:22 -0500

    update session amount

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -66,10 +66,9 @@ const webhookCheckout = catchAsync(async (req, res, next) => {
   }
 
   const createBookingSession = catchAsync(async (session) => {
-    console.log('session:', session);
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].price_data.unit_amount;
+    const price = session.amount_total / 100;
     await Booking.create({ tour, user, price });
   });
 
